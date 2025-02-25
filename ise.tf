cabb8f2d5455afed2a0f1b745c0779384cf8579c
Greg Gibbs <103554967+grg1bbs@users.noreply.github.com>
2025-02-25 15:33:21 +1100

    Update ise.tf for module re-factor
    Updated code to use new child module structure

--- ise.tf
+++ ise.tf
@@ -1,61 +1,52 @@
 
 # Create network interface for ISE - AZ1
-resource "aws_network_interface" "ise31aws1_gig0" {
+module "ise34aws1_gig0" {
+  source          = "./modules/network-interface"
   subnet_id       = aws_subnet.private_subnet[0].id
-  private_ips     = [var.ise31_aws1_gig0_ip]
+  private_ips     = [var.ise_aws1_gig0_ip]
   security_groups = [aws_security_group.ise_network_access.id]
+  tags = {
+    Name = "ise34aws1_gig0"
+  }
 }
+
 # Create network interface for ISE - AZ2
-resource "aws_network_interface" "ise31aws2_gig0" {
+module "ise34aws2_gig0" {
+  source          = "./modules/network-interface"
   subnet_id       = aws_subnet.private_subnet[1].id
-  private_ips     = [var.ise31_aws2_gig0_ip]
+  private_ips     = [var.ise_aws2_gig0_ip]
   security_groups = [aws_security_group.ise_network_access.id]
+  tags = {
+    Name = "ise34aws2_gig0"
+  }
 }
 
-# Create two ISE instances in the private subnets
-
-resource "aws_instance" "ise31aws1" {
-  ami               = var.ise_ami_apse2
+# Create ISE instance = AZ1
+module "ise34aws1" {
+  source = "./modules/ise"
+  ami               = var.ise34_ami_apse2
   availability_zone = var.iselab_az1
   instance_type     = var.ise_instance_type_small
   key_name          = var.key_name
-  network_interface {
-    network_interface_id = aws_network_interface.ise31aws1_gig0.id
-    device_index         = 0
-  }
+  network_interface_id = module.ise34aws1_gig0.private_ip.id
+  volume_size = var.volume_size
   tags = {
-    "Name" = "ise31aws1"
+    Name = "ise34aws1"
   }
-  user_data = file("./ise31aws1.txt")
-  lifecycle {
-    ignore_changes = [user_data]
-  }
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 300
-    volume_type = "gp2"
-  }
+  user_data = file("./ise34aws1.txt")
 }
 
-resource "aws_instance" "ise31aws2" {
-  ami               = var.ise_ami_apse2
+# Create ISE instance = AZ2
+module "ise34aws2" {
+  source = "./modules/ise"
+  ami               = var.ise34_ami_apse2
   availability_zone = var.iselab_az2
   instance_type     = var.ise_instance_type_small
   key_name          = var.key_name
-  network_interface {
-    network_interface_id = aws_network_interface.ise31aws2_gig0.id
-    device_index         = 0
-  }
+  network_interface_id = module.ise34aws2_gig0.private_ip.id
+  volume_size = var.volume_size
   tags = {
-    "Name" = "ise31aws2"
+    Name = "ise34aws2"
   }
-  user_data = file("./ise31aws2.txt")
-    lifecycle {
-    ignore_changes = [user_data]
-  }
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 300
-    volume_type = "gp2"
-  }
+  user_data = file("./ise34aws2.txt")
 }
